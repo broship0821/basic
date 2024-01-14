5f8c429854dae9ab29d55c462acbfff608f608a8
peter <gudqo1995@naver.com>
2024-01-15 00:32:52 +0900

    first commit of study react

---
+++ src/AppXY.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./AppXY.css";
+
+export default function AppXY() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const mouseMove = (event) => {
+    const mouseX = event.pageX;
+    const mouseY = event.pageY;
+    setX(mouseX);
+    setY(mouseY);
+  };
+  return (
+    <div className="container" onMouseMove={mouseMove}>
+      <div
+        className="pointer"
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+      ></div>
+    </div>
+  );
+}
