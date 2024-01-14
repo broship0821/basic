911c7901637fecd6534bf605c12525e44d909fb8
Peter <broship0821@buptle.com>
2024-01-15 01:08:43 +0900

    working in other computer

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  // for test commit;
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
   const mouseMove = (event) => {
-    const mouseX = event.pageX;
-    const mouseY = event.pageY;
-    setX(mouseX);
-    setY(mouseY);
+    const x = event.pageX;
+    const y = event.pageY;
+    // setPosition({ x, y }); // 마우스 옮기는 대로 이동
+    // 수평으로만 이동
+    setPosition((prev) => ({ ...prev, x }));
   };
   return (
     <div className="container" onMouseMove={mouseMove}>
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       ></div>
     </div>
   );
